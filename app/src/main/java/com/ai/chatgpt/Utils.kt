168eb8f783e4a1c12322b92b62c30c40639a3755
Prasidh Gopal Anchan <92362239+Kawaki22@users.noreply.github.com>
2023-09-05 21:54:17 +0530

    Update Utils.kt

--- app/src/main/java/com/ai/chatgpt/Utils.kt
+++ app/src/main/java/com/ai/chatgpt/Utils.kt
@@ -1,7 +1,7 @@
 package com.ai.chatgpt
 
 object Utils {
-    const val API_KEY = "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"
+    const val API_KEY = "KEY"
     const val BASE_URL = "https://api.openai.com/"
     const val CONTENT_TYPE = "application/json"
 }
